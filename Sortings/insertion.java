2395eb324b97ec03b2e2ba2f945837cff28eea19
Abhishek.hit <abhishek539700@gmail.com>
2026-02-14 14:25:28 +0530

    solve

--- Sortings/insertion.java
+++ Sortings/insertion.java
@@ -3,8 +3,8 @@ package Sortings;
 public class insertion {
     public static void main(String[] args) {
         int[] arr={5,5,12,42,36,45,8,4};
-        for (int i=0;i< arr.length-1;i++){
-            int curr=i;
+        for (int i=1;i< arr.length;i++){
+            int curr=arr[i];
             int prev=i-1;
             while (prev>=0&&arr[prev]>curr){
                 arr[prev+1]=arr[prev];
