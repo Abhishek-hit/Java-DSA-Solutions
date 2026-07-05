b513f57882271bb809a722e432752a6920c7ec6a
Abhishek.hit <abhishek539700@gmail.com>
2026-07-05 13:35:57 +0530

    solve another prolem

---
+++ Sheet/Q35.java
@@ -0,0 +1,36 @@
+package Sheet;
+
+import java.util.Scanner;
+
+public class Q35 {
+    public static void main(String[] args) {
+       // String str="madam";
+        String str;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the string");
+       if (sc.hasNextInt ()){
+           System.out.println ("pls enter string only" );
+           return;// yahi pr program end ho gya
+       }else {
+           str = sc.nextLine ( );
+
+       }
+        char[] ch=str.toCharArray ();
+        int n= ch.length;
+        int l=0;
+        int r=n-1;
+        while (l<r){
+            char temp=ch[l];
+            ch[l]=ch[r];
+            ch[r]=temp;
+            l++;
+            r--;
+        }
+        String result=new String ( ch );
+      if (result.equals (str)){
+          System.out.println ("palindrom" );
+      }else {
+          System.out.println ("not palindrom" );
+      }
+    }
+}
