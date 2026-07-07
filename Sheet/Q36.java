2a9edf5986697d9c04dd5cb2748aeb0baf060f75
Abhishek.hit <abhishek539700@gmail.com>
2026-07-07 22:30:56 +0530

    pattern question solve

--- Sheet/Q36.java
+++ Sheet/Q36.java
@@ -2,27 +2,33 @@ package Sheet;
 
 public class Q36 {
     public static void main(String[] args) {
-        String str="banana";
-        char[] chars=str.toCharArray ();
-        int n=chars.length;
-        boolean[] visited=new boolean[n];
-        for (int i=0;i<n;i++){
-            if (visited[i]){
-                continue;
-            }
-            int count=1;
-            for (int j=i+1;j<n;j++){
-                if (chars[i]==chars[j]){
-                    count++;
-                    visited[j]=true;
-                }
-            }
-//            System.out.println(chars[i] + " = " + count);
-            if (count>1){
-                System.out.println(chars[i] + " = " + count);
-            }
-        }
+//        String str="banana";
+//        char[] chars=str.toCharArray ();
+//        int n=chars.length;
+//        boolean[] visited=new boolean[n];
+//        for (int i=0;i<n;i++){
+//            if (visited[i]){
+//                continue;
+//            }
+//            int count=1;
+//            for (int j=i+1;j<n;j++){
+//                if (chars[i]==chars[j]){
+//                    count++;
+//                    visited[j]=true;
+//                }
+//            }
+////            System.out.println(chars[i] + " = " + count);
+//            if (count>1){
+//                System.out.println(chars[i] + " = " + count);
+//            }
+//        }
 
+        for (int i=0;i<5; i++){
+            for (int j=0;j<i;j++){
+                System.out.print (" *" );
+            }
+            System.out.println (  );
+        }
 
     }
 }
