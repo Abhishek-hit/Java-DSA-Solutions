127a146bfcc4365fcf442e5e7ae235ce0857273c
Abhishek.hit <abhishek539700@gmail.com>
2026-07-13 22:57:38 +0530

    diamond pattern

---
+++ Sheet/Q36I.java
@@ -0,0 +1,15 @@
+package Sheet;
+
+public class Q36I {
+    public static void main(String[] args) {
+        for (int i=0;i<5; i++){
+            for (int j=4;j>i;j--){
+                System.out.print (" ");
+            }
+            for (int j=0;j<=i;j++){
+                System.out.print(" *" );
+            }
+            System.out.println (  );
+        }
+    }
+}
