3b8166714ca68645aac4698049af37976f3b1725
Abhishek.hit <abhishek539700@gmail.com>
2026-08-13 12:31:57 +0530

    solve new pattern in dsa

--- Sheet/Q29.java
+++ Sheet/Q29.java
@@ -5,24 +5,25 @@ public class Q29 {
         String str="hello";
         char[] ch=str.toCharArray ();
         int n= ch.length;
-        String rev=" ";
-//        int i=0;
-//        int j=n-1;
-//        while (i<j){
-//            char temp=ch[i];
-//            ch[i]=ch[j];
-//            ch[j]=temp;
-//            i++;
-//            j--;
-//        }
-//
+      //  String rev=" ";
+        int i=0;
+        int j=n-1;
+        while (i<j){
+            char temp=ch[i];
+            ch[i]=ch[j];
+            ch[j]=temp;
+            i++;
+            j--;
+        }
+        System.out.println (ch );
+
 //        String rev=new String ( ch );
 //        System.out.println (rev );
 
-        for (int i=0;i<=n-1;i++){
-            rev=ch[i]+rev;
-        }
-        System.out.println (rev );
+//        for (int i=0;i<=n-1;i++){
+//            rev=ch[i]+rev;
+//        }
+//        System.out.println (rev );
     }
 
 }
--- Sheet/Q36.java
+++ Sheet/Q36.java
@@ -2,36 +2,36 @@ package Sheet;
 
 public class Q36 {
     public static void main(String[] args) {
-//        String str="banana";
-//        char[] chars=str.toCharArray ();
-//        int n=chars.length;
-//        boolean[] visited=new boolean[n];
-//        for (int i=0;i<n;i++){
-//            if (visited[i]){
-//                continue;
-//            }
-//            int count=1;
-//            for (int j=i+1;j<n;j++){
-//                if (chars[i]==chars[j]){
-//                    count++;
-//                    visited[j]=true;
-//                }
-//            }
-////            System.out.println(chars[i] + " = " + count);
-//            if (count>1){
-//                System.out.println(chars[i] + " = " + count);
-//            }
-//        }
-
-        for (int i=0;i<5; i++){
-            for (int j=4;j>i;j--){
-                System.out.print (" ");
+        String str="banana";
+        char[] chars=str.toCharArray ();
+        int n=chars.length;
+        boolean[] visited=new boolean[n];
+        for (int i=0;i<n;i++){
+            if (visited[i]){
+                continue;
             }
-            for (int j=0;j<=i;j++){
-                System.out.print("*" );
+            int count=1;
+            for (int j=i+1;j<n;j++){
+                if (chars[i]==chars[j]){
+                    count++;
+                    visited[j]=true;
+                }
             }
-            System.out.println (  );
+//            System.out.println(chars[i] + " = " + count);
+            if (count>1){
+                System.out.print(chars[i]+" ");
+            }
         }
+
+//        for (int i=0;i<5; i++){
+//            for (int j=4;j>i;j--){
+//                System.out.print (" ");
+//            }
+//            for (int j=0;j<=i;j++){
+//                System.out.print("*" );
+//            }
+//            System.out.println (  );
+//        }
 
     }
 }
